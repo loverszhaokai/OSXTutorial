1747c8dbf0483f68edeb8f9bf5408f9b5e85865c
Kai Zhao <loverszhao@gmail.com>
2016-05-10 21:59:29 +0800

    Initial Commit

---
+++ window_test_1/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  window_test_1
+//
+//  Created by zhaokai on 5/10/16.
+//  Copyright (c) 2016 zhaokai. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
