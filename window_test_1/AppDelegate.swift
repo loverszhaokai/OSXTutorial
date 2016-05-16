e9019b76e75c4c062014ae1eda9fe05aecafaa86
Kai Zhao <loverszhao@gmail.com>
2016-05-16 21:06:03 +0800

    Add window_test_1

--- window_test_1/AppDelegate.swift
+++ window_test_1/AppDelegate.swift
@@ -12,10 +12,27 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    
+    let overlayWindow = NSWindow()
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        let screen = NSScreen.mainScreen()
+        let rect: NSRect = screen!.frame
+        let height = rect.size.height
+        let width = rect.size.width
+        var frame = screen!.frame
+        frame.size = NSMakeSize(width, height)
+        
+        overlayWindow.setFrame(frame, display: true)
+        overlayWindow.titlebarAppearsTransparent = true
+        overlayWindow.titleVisibility = NSWindowTitleVisibility.Hidden
+        overlayWindow.styleMask |= NSFullSizeContentViewWindowMask
+        overlayWindow.styleMask |= NSBorderlessWindowMask
+        overlayWindow.level = 10000000
+        overlayWindow.setIsVisible(true)
+        overlayWindow.backgroundColor = NSColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.7)
+        overlayWindow.movable = false
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
@@ -24,4 +41,3 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
 }
-
